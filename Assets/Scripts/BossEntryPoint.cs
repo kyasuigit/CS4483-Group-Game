854e8396ce71a6f5736fb6da341d09d5e655fc71
Kohei Yasui <kyasui@uwo.ca>
2024-04-02 02:53:42 -0400

    added boss entry camer movement

---
+++ Assets/Scripts/BossEntryPoint.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BossEntryPoint : MonoBehaviour
+{
+    public Camera cameraPan;
+    public GameObject leftWall;
+
+    public Tilemap tilemap;
+    public float duration = 1.3f;
+
+    private bool wallIsAppearing = false;
+
+    private float currentAlpha = 0f;
+    private float timer = 0f;
+    private Color startingColor;
+    private Color targetColor;
+
+    private void Start()
+    { 
+        startingColor = tilemap.color;
+        targetColor = new Color(startingColor.r, startingColor.g, startingColor.b, 1f);
+    }
+
+    private void Update()
+    {
+        if (wallIsAppearing ) { 
+            timer += Time.deltaTime;
+
+            float t = Mathf.Clamp01(timer / duration);
+            tilemap.color = Color.Lerp(startingColor, targetColor, t);
+
+            if (t >= 1f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            if (!wallIsAppearing) { 
+                cameraPan.GetComponent<CameraFollow>().BossTime();
+                leftWall.SetActive(true);
+                wallIsAppearing = true;
+            }
+        }
+    }
+}
